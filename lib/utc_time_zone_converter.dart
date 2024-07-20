27e6fc16ebb75273b9fc5cc9d83bf8c5f317bd8f
cyrax <bskaxe@gmail.com>
2024-07-20 16:59:55 +0400

    time_zone_factory export

--- lib/utc_time_zone_converter.dart
+++ lib/utc_time_zone_converter.dart
@@ -7,3 +7,4 @@ export 'src/offset_parser/offset_parser.dart';
 export 'src/offset_parser/utc_offset_parser.dart';
 export 'src/time_zone.dart';
 export 'src/time_zone_converter.dart';
+export 'src/time_zone_factory.dart';
